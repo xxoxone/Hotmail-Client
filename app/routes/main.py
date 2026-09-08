2a51f3955bfef404e74a6aa00e2489968b0572a6
moon <hello@xxox.one>
2026-09-08 18:44:37 +0600

    Update main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
 
 bp_main = Blueprint('main', __name__)
 
@@ -21,3 +21,62 @@ def route_notepad():
 @bp_main.route('/gmail-dot')
 def route_gmail_dot():
     return render_template('under_construction.html', active_tool='gmail-dot', page_title='Gmail Dot Trick Generator')
+
+@bp_main.route('/robots.txt')
+def robots():
+    content = """User-agent: *
+Allow: /
+Sitemap: https://microtools.one/sitemap.xml
+"""
+    return Response(content, mimetype="text/plain")
+
+@bp_main.route('/sitemap.xml')
+def sitemap():
+    content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://microtools.one/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/hotmail</loc>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/2fa</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/tiktok</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/fb-check</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/fb-uid</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/x-downloader</loc>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/fb-id</loc>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/tempmail</loc>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/notepad</loc>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>https://microtools.one/gmail-dot</loc>
+    <priority>0.5</priority>
+  </url>
+</urlset>"""
+    return Response(content, mimetype="application/xml")
